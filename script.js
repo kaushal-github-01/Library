909a05f7862e0faed87f4061649df8e68b1a410c
kaushal-github-01 <Kaushalkundu5683@gmail.com>
2023-05-05 19:05:09 +0530

    added 'update' button

--- script.js
+++ script.js
@@ -18,24 +18,45 @@ addBookButton.onclick = () => {
 
 // DIsplaying it on the screen
 
-displayBook = (arr) => {
-    let lastElement = arr[arr.length - 1];
+displayBook = (book, index) => {
     let div = document.createElement('div');
+    div.setAttribute('data-index', index);
 
     let removeButton = document.createElement('button');
     removeButton.textContent = 'Remove';
     removeButton.setAttribute('id', 'removeButton');
-    removeButton.setAttribute('style', 'margin-top: 5px;');
+    removeButton.setAttribute('style', 'margin: 5px 0px 0px 5px;');
     removeButton.onclick = () => {
         div.style.display = "none";
     }
 
-    for (const key in lastElement) {
-        div.innerHTML += `${key}: ${lastElement[key]} <br>`;
+    for (const key in book) {
+        let value = book[key];
+        if (key === "Read") {
+            value = `<span class="read-value">${value}</span>`;
+        }
+        div.innerHTML += `${key}: ${value} <br>`;
     }
+
+    let updateButton = document.createElement('button');
+    updateButton.textContent = "Update";
+    updateButton.onclick = () => {
+        let index = div.getAttribute('data-index');
+        let book = myLibrary[index];
+
+        book.Read = book.Read === 'Yes' ? 'No' : 'Yes';
+
+        // update value on screen
+        let readValueSpan = div.querySelector('.read-value');
+        readValueSpan.textContent = book.Read;
+    }
+
+    div.appendChild(updateButton);
     div.appendChild(removeButton);
     books.appendChild(div);
 }
+
+
 
 submitButton.onclick = (event) => {
     event.preventDefault();
@@ -46,10 +67,6 @@ submitButton.onclick = (event) => {
     const pageInput = document.getElementById('page');
     const yesRadio = document.getElementById("yes-radio");
     const noRadio = document.getElementById("no-radio");
-
-    const name = nameInput.value;
-    const author = authorInput.value;
-    const page = pageInput.value;
 
     // get selected radio button value
     let isRead;
@@ -62,9 +79,14 @@ submitButton.onclick = (event) => {
     }
 
     // create new book object and add to library
-    const book = new Book(name, author, page, isRead);
+    const book = new Book(nameInput.value, authorInput.value, pageInput.value, isRead);
     myLibrary.push(book);
-    displayBook(myLibrary);
+
+    // get index of the new book object in myLibrary array
+    const index = myLibrary.indexOf(book);
+
+    // pass book object and index to displayBook function
+    displayBook(book, index);
 
     // clear input fields
     nameInput.value = "";
